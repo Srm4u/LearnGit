b53a48f631f5e05ddbf9aab72eaac3d1c5f8eacf
Srm4u <srmohanty4u@gmail.com>
2019-01-05 11:08:52 +0530

    Auto IT

---
+++ src/Authentication.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Authentication {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+
+			System.setProperty("webdriver.chrome.driver","E:\\sel\\autoIT\\chromedriver.exe");
+			
+			WebDriver driver = new ChromeDriver();
+			
+			driver.get("https://www.engprod-charter.net");
+			
+			Runtime.getRuntime().exec("E:\\sel\\autoIT\\Ranjan\\Authentication.exe");
+
+	}
+
+}
